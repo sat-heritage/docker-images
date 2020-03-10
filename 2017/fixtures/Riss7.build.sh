ce5c15f9ead8a6f69ca8d532d0b56e876cbe19f0
Gilles <gilles.audemard@gmail.com>
2020-03-10 11:34:35 +0100

    Riss7 is ok

---
+++ 2017/fixtures/Riss7.build.sh
@@ -0,0 +1,10 @@
+#set -x
+#mkdir debug
+#cd debug
+#cmake -D DRATPROOF=ON ..
+#make riss-simp
+
+#make riss-core
+
+# Copy all scripts to the bin/ directory
+#make scripts
--- 2017/fixtures/Riss7.pre_build.sh
+++ 2017/fixtures/Riss7.pre_build.sh
@@ -1,1 +1,11 @@
 mv riss7 Riss7
+set -x
+echo "mkdir debug" > Riss7/starexec_build
+echo "mkdir bin" >> Riss7/starexec_build
+echo "cd debug" >> Riss7/starexec_build
+echo "cmake -D DRATPROOF=ON .. " >> Riss7/starexec_build
+echo "make riss-simp" >> Riss7/starexec_build
+echo "make riss-core" >> Riss7/starexec_build
+
+# Copy all scripts to the bin/ directory
+echo "cp bin/* ../bin" >> Riss7/starexec_build
