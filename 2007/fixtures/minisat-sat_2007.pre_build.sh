4b68a08b4e8ac75f20cc29b4e770a725d1fc09fa
Gilles <gilles.audemard@gmail.com>
2020-03-16 18:49:46 +0100

    fix minisat_2007

---
+++ 2007/fixtures/minisat-sat_2007.pre_build.sh
@@ -0,0 +1,4 @@
+echo "cd simp" >  /src/minisat-sat07/build.sh
+echo "make rs" >>  /src/minisat-sat07/build.sh
+echo "cp minisat_static ../bin" >> /src/minisat-sat07/build.sh
+mkdir /src/minisat-sat07/bin
